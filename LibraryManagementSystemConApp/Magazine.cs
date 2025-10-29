ccf9b88f31ec6d543540a7bc817d6ef412436831
Anik Chanra Deb <anik.deb161@gmail.com>
2025-10-29 23:25:31 +0600

    feat: added magazine derived class from library item base class

---
+++ LibraryManagementSystemConApp/Magazine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystemConApp
+{
+    class Magazine : LibraryItem
+    {
+        public int IssueNumber { get; set; }
+
+        public Magazine(string title, int itemId, int issueNumber, bool isAvailable = true)
+            : base(title, itemId, isAvailable)
+        {
+            IssueNumber = issueNumber;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"[Magazine] ID: {ItemId}, Title: {Title}, Issue: {IssueNumber}, Available: {IsAvailable}");
+        }
+    }
+}
