a31e39988774a484964244a6f3e458e3fc1ccad5
Anik Chanra Deb <anik.deb161@gmail.com>
2025-10-29 23:24:41 +0600

    feat: added library item base class

---
+++ LibraryManagementSystemConApp/LibraryItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystemConApp
+{
+    class LibraryItem
+    {
+        public string Title { get; set; }
+        public int ItemId { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public LibraryItem(string title, int itemId, bool isAvailable = true)
+        {
+            Title = title;
+            ItemId = itemId;
+            IsAvailable = isAvailable;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {ItemId}, Title: {Title}, Available: {IsAvailable}");
+        }
+    }
+}
