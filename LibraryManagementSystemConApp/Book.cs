7a35697c2f6a142cce55b515877f52b32fdc8c8f
Anik Chanra Deb <anik.deb161@gmail.com>
2025-10-29 23:25:05 +0600

    feat: added book derived class from library item

---
+++ LibraryManagementSystemConApp/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystemConApp
+{
+    class Book : LibraryItem
+    {
+        public string Author { get; set; }
+
+        public Book(string title, int itemId, string author, bool isAvailable = true)
+            : base(title, itemId, isAvailable)
+        {
+            Author = author;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"[Book] ID: {ItemId}, Title: {Title}, Author: {Author}, Available: {IsAvailable}");
+        }
+    }
+}
