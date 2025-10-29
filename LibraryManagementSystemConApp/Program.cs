843651ad37e6f8ef7b6df471fb8158b856340bab
Anik Chanra Deb <anik.deb161@gmail.com>
2025-10-29 23:26:00 +0600

    feat: added main class and demonstrate

--- LibraryManagementSystemConApp/Program.cs
+++ LibraryManagementSystemConApp/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book1 = new Book("Valo Achi", 101, "AC. Deb");
+            Book book2 = new Book("Valo Theko", 102, "AC. Deb");
+
+            Magazine mag1 = new Magazine("Tech Today", 201, 45);
+
+            Console.WriteLine("=== Library Items ===");
+            book1.DisplayInfo();
+            book2.DisplayInfo();
+            mag1.DisplayInfo();
+
+            Console.WriteLine("\nBorrowing");
+            book2.IsAvailable = false;
+
+            Console.WriteLine("\n=== Updated Library Items ===");
+            book1.DisplayInfo();
+            book2.DisplayInfo();
+            mag1.DisplayInfo();
         }
     }
 }
